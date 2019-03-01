aa3d926954e24b9bae379aa0ea1a13e5cad98cbe
macjabeth <macbethjonathan@gmail.com>
2019-03-01 18:12:22 +0000

    include router with separate routes for smurfs and smurf form

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import axios from 'axios';
 
 import './App.css';
@@ -30,8 +31,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf} />
-        <Smurfs smurfs={this.state.smurfs} />
+        <Route
+          exact
+          path="/"
+          render={() => <Smurfs smurfs={this.state.smurfs} />}
+        />
+        <Route
+          path="/smurf-form"
+          render={() => <SmurfForm addSmurf={this.addSmurf} />}
+        />
       </div>
     );
   }
